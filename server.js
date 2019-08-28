e3323d8835bd06f9df81f79dc9a649f624c87e5d
montu <montu@gmail.com>
2019-08-28 18:53:04 +0530

    .

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const app=express()
 
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('bef63e1bb3b84f68a62bc31d486bd332');
-    
+
+app.set('view engine','html')
+
 app.get("/",(req,res)=>{
-    newsapi.v2.topHeadlines({
+    newsapi.v2.everything({
         sources: 'bbc-news,bbc-sport,abc-news',
         q:'uk',
         language: 'en'
     }).then(response => {
-    res.send(response.articles[0].source);
-    console.log(response.articles[0].source)
+        res.render('index')
+        const list=document.getElementById('list')
+        var kk=response.articles;
+        var len=kk.length
+        console.log(len)
+        for(var i=0;i<len;i++)
+        {
+            list.innerHTML+=response.articles[i].title
+            console.log(response.articles[i].title)
+        }
 }).catch(err=>{
     console.log(err);
 })
