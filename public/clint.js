598ef9d10b55cea6eab8f7c57fae057c0664418e
montu <montu@gmail.com>
2019-09-12 19:17:29 +0530

    .

--- public/clint.js
+++ public/clint.js
@@ -5,6 +5,5 @@ console.log(frame)
 for(let i=0;i<btn.length;i++)
 {
     btn[i].addEventListener('click',function(event){
-    frame.innerHTML=`<iframe scr="${event.target.dataset.url}" class="fra"></iframe>`
     })
 }
